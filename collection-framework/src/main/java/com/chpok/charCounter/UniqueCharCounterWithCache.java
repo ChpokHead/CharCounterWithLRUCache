6bf014d4b3e722b81002843a4af542b8594c0de9
mildgeneral@gmail.com <Ihsan2019>
2021-02-07 18:55:49 +0300

    add builder pattern

--- collection-framework/src/main/java/com/chpok/charCounter/UniqueCharCounterWithCache.java
+++ collection-framework/src/main/java/com/chpok/charCounter/UniqueCharCounterWithCache.java
@@ -1,7 +1,8 @@
 package com.chpok.charCounter;
 
 import java.util.LinkedHashMap;
-import com.chpok.charCounter.cache.CounterCache;
+
+import com.chpok.charCounter.cache.CounterCacheProvider;
 import com.chpok.charCounter.formatter.CharCounterViewProvider;
 import com.chpok.charCounter.provider.CharCounterProvider;
 import com.chpok.charCounter.validation.ValidationProvider;
@@ -10,27 +11,64 @@ public class UniqueCharCounterWithCache {
     private final ValidationProvider validationProvider;
     private final CharCounterProvider charCounterProvider;
     private final CharCounterViewProvider charCounterViewProvider;
-    private final CounterCache cache;
+    private final CounterCacheProvider counterCacheProvider;
 
-    public UniqueCharCounterWithCache(ValidationProvider validationProvider, CharCounterProvider charCounterProvider, CharCounterViewProvider charCounterViewProvider) {
-        this.validationProvider = validationProvider;
-        this.charCounterProvider = charCounterProvider;
-        this.charCounterViewProvider = charCounterViewProvider;
-        cache = new CounterCache();
+    public UniqueCharCounterWithCache(Builder builder) {
+        validationProvider = builder.validationProvider;
+        charCounterProvider = builder.charCounterProvider;
+        charCounterViewProvider = builder.charCounterViewProvider;
+        counterCacheProvider = builder.counterCacheProvider;
+    }
+    
+    public static Builder builder() {
+        return new Builder();
     }
     
     public String countUniqueCharacters(String sentence) {
         validationProvider.validate(sentence);
         
-        LinkedHashMap<Character, Integer> result;
+        final LinkedHashMap<Character, Integer> characterToCount;
         
-        if (cache.isSentenceAlreadyCounted(sentence)) {
-            result = cache.getAlreadyCountedSentenceCounter(sentence);
+        if (counterCacheProvider.isSentenceAlreadyCounted(sentence)) {
+            characterToCount = counterCacheProvider.getAlreadyCountedSentenceCounter(sentence);
         } else {
-            result = charCounterProvider.provideCharCount(sentence);
-            cache.addCountedSentence(sentence, result);
+            characterToCount = charCounterProvider.provideCharCount(sentence);
+            counterCacheProvider.addCountedSentence(sentence, characterToCount);
         }
         
-        return charCounterViewProvider.provideView(result);
+        return charCounterViewProvider.provideView(characterToCount);
+    }
+    
+    public static class Builder {
+        private ValidationProvider validationProvider;
+        private CharCounterProvider charCounterProvider;
+        private CharCounterViewProvider charCounterViewProvider;
+        private CounterCacheProvider counterCacheProvider;
+        
+        private Builder() {}
+        
+        public Builder withValidation(ValidationProvider validationProvider) {
+            this.validationProvider = validationProvider;
+            return this;
+        }
+        
+        public Builder withCharCounter(CharCounterProvider charCounterProvider) {
+            this.charCounterProvider = charCounterProvider;
+            return this;
+        }
+        
+        public Builder withCharCounterView(CharCounterViewProvider charCounterViewProvider) {
+            this.charCounterViewProvider = charCounterViewProvider;
+            return this;
+        }
+        
+        public Builder withCounterCache(CounterCacheProvider counterCacheProvider) {
+            this.counterCacheProvider = counterCacheProvider;
+            return this;
+        }
+        
+        public UniqueCharCounterWithCache build() {
+            return new UniqueCharCounterWithCache(this);
+        }
     }
 }
