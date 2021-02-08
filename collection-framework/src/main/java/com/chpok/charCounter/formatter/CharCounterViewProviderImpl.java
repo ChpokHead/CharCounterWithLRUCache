e0a600994e1845ac8c77807b214a01060e6f4552
mildgeneral@gmail.com <Ihsan2019>
2021-02-08 19:14:39 +0300

    add string format usage

--- collection-framework/src/main/java/com/chpok/charCounter/formatter/CharCounterViewProviderImpl.java
+++ collection-framework/src/main/java/com/chpok/charCounter/formatter/CharCounterViewProviderImpl.java
@@ -3,16 +3,11 @@ package com.chpok.charCounter.formatter;
 import java.util.Map;
 
 public class CharCounterViewProviderImpl implements CharCounterViewProvider{
-    private static final String QUOTE = "\"";
-    private static final String DASH = "-";
-    private static final String SPACE = " ";
-    private static final String NEWLINE = "\n";
-    
     @Override
     public String provideView(Map<Character, Integer> characterToCount) {
         final StringBuilder builder = new StringBuilder();
-        
-        characterToCount.forEach((sym, count) -> builder.append(QUOTE + sym + QUOTE + SPACE + DASH + SPACE + count + NEWLINE));
+
+        characterToCount.forEach((sym, count) -> builder.append(String.format("\"%c\" - %d%n", sym, count)));
         
         return builder.toString();
     }
